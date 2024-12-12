d1ba30d24da4c1829cb5587fda6267f847cdf17a
Joan Henao <henaobermeosteven@gmail.com>
2024-12-11 22:29:29 -0500

    Creacion Clase AlertService

---
+++ LabScript/AlertService.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.JSInterop;
+
+namespace LabScript
+{
+	public class AlertService : IAsyncDisposable
+	{
+		readonly Lazy<Task<IJSObjectReference>> ijsObjectReference;
+
+		public AlertService(IJSRuntime ijsRuntime)
+		{
+			this.ijsObjectReference = new Lazy<Task<IJSObjectReference>>();
+		}
+
+		public ValueTask DisposeAsync()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
